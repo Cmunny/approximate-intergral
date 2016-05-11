1cfe2304bb6e019b2fddd5b8d78cbec9075fdfc9
Cmunny <connor-carpenter@comcast.net>
2016-05-10 18:54:46 -0700

    Cleaned up .h file

---
+++ ApproximateIntergral.h
@@ -0,0 +1,24 @@
+#ifndef INTEGRAL_H
+#define INTEGRAL_H
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ApprIntegral
+{
+  static const string trig[];
+  static vector<string> TermSeperater(string str);
+  static double CalcPolynomial(string term, double num);
+  static double CalcTrig(string term, double num, int trigFuncIndex);
+  static double termCalc(string term, double num);
+  ApprIntegral(){};
+public:
+  static double calcEquation(string equation, double num);
+  static double approximateIntegral(string equation, double a, double b, int numOfRects);
+};
+
+
+#endif
